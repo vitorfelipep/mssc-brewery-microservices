ccddceda4c3f62663ecdd7ca1f6cfaadfdca19b7
Vitor Costa <vitor.costa@redspark.io>
2020-11-29 18:45:24 -0300

    Commit initial

---
+++ src/main/java/com/learn/springframework/msscbrewer/MsscBrewerApplication.java
@@ -0,0 +1,13 @@
+package com.learn.springframework.msscbrewer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscBrewerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsscBrewerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/learn/springframework/msscbrewer/MsscBrewerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.springframework.msscbrewer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBrewerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
